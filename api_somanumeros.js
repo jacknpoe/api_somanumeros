eaa8a6bd7ec80f4cae075aca1aa99d42232fa80f
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-01-13 09:39:14 -0300

    Add files via upload

--- api_somanumeros.js
+++ api_somanumeros.js
@@ -1,9 +1,3 @@
-//***********************************************************************************************
-// AUTOR: Ricardo Erick Rebêlo
-// Objetivo: consome a API de retorno de JSON com o resultado da soma de três números
-// Alterações:
-// 0.1   13/01/2024 - primeira implementação
-
 const numero1 = window.document.getElementById("numero1")
 const numero2 = window.document.getElementById("numero2")
 const numero3 = window.document.getElementById("numero3")
@@ -12,34 +6,15 @@ const botao = window.document.getElementById("botao")
 
 botao.addEventListener("click", (evt) => {
     // converte em float, trocando vírgula por ponto antes
-    var float1 = Number.parseFloat( numero1.value.replace(",", "."));
-    var float2 = Number.parseFloat( numero2.value.replace(",", "."));
-    var float3 = Number.parseFloat( numero3.value.replace(",", "."));
-
-    // avalia se a, b e c são realmente números, para não travar o script
-    if( isNaN(float1)) {
-        alert("O valor de a não é um número!");
-        tn1.focus();
-        return;
-    }
-
-    if( isNaN(float2)) {
-        alert("O valor de b não é um número!");
-        tn2.focus();
-        return;
-    }
-
-    if( isNaN(float3)) {
-        alert("O valor de c não é um número!");
-        tn3.focus();
-        return;
-    }
+    const int1 = Number.parseInt( numero1.value);
+    const int2 = Number.parseInt( numero2.value);
+    const int3 = Number.parseInt( numero3.value);
 
     // dados enviados para a API (a, b, c)
     const dados = {
-        valor1: float1,
-        valor2: float2,
-        valor3: float3
+        valor1: int1,
+        valor2: int2,
+        valor3: int3
     }
 
     // cabeçalho indica que é post e os dados no corpo
--- api_somanumeros.php
+++ api_somanumeros.php
@@ -1,17 +1,10 @@
 <?php
-	//***********************************************************************************************
-	// AUTOR: Ricardo Erick Rebêlo
-	// Objetivo: API de retorno de JSON da soma de três números
-	// Alterações:
-	// 0.1   13/01/2024 - primeira implementação
-
 	header('Content-Type: application/json');
 
 	$json = file_get_contents('php://input');		// pega o JSON da requisição
-	$data = json_decode($json);		// decodifica para objeto
+	$dados = json_decode($json);		// decodifica para objeto
 
-	$resultado = $data->valor1 + $data->valor2 + $data->valor3;
-	$resultado = str_replace( '.', ',', strval( $resultado));
+	$resultado = $dados->valor1 + $dados->valor2 + $dados->valor3;
 
 	echo json_encode($resultado);
 ?>
